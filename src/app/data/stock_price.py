87446987b665fdea43501f1fe4bcffa7fc2fc521
Douglas R. Ferreira <drfdouglas@hotmail.com>
2021-02-08 22:42:51 -0300

    Volume formatting

--- src/app/data/stock_price.py
+++ src/app/data/stock_price.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+import re
 
 class StockPrice():
 
@@ -10,6 +10,20 @@ class StockPrice():
         self.close = close
         self.high = high
         self.low = low
-        self.volume = volume
+        self.volume = self.__parse_and_format_volume_value(volume)
 
         return
+
+    def __parse_and_format_volume_value(self, value):
+        volume_regex =  re.compile(r'(\d+|,)|(K|M|B)')
+        m = volume_regex.match(value)
+        value = float(m.group(0))
+        sufix = str(m.group(1))
+        
+        if(sufix == 'K' or sufix == 'k'):
+            return value * 1000
+        elif(sufix == 'M' or sufix == 'm'):
+            return value * 1000000
+        elif(sufix == 'B' or sufix == 'b'):
+            return value * 1000000000
+        
