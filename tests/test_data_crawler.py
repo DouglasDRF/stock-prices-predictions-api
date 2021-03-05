cb4e9c01a66a506a708490cdf40bd13bfa79a31c
Douglas R. Ferreira <drfdouglas@hotmail.com>
2021-03-05 20:11:38 -0300

    Fixing tests

--- tests/test_data_crawler.py
+++ tests/test_data_crawler.py
@@ -9,15 +9,13 @@ class TestCrawler:
 
         crawler = StocksCrawler(ticker='BBDC4', source='https://www.infomoney.com.br/cotacoes/bradesco-bbdc4/historico/')
         result = crawler.get_last_daily_price()    
-        
-        assert len(result) == 1
-        
-        assert result[0].ticker == 'BBDC4'
-        assert result[0].open != 0.0
-        assert result[0].close != 0.0
-        assert result[0].high != 0.0
-        assert result[0].low != 0.0
-        assert result[0].low != 0 
+     
+        assert result.ticker == 'BBDC4'
+        assert result.open != 0.0
+        assert result.close != 0.0
+        assert result.high != 0.0
+        assert result.low != 0.0
+        assert result.low != 0 
         
 
     def test_get_history_infomoney(self):
@@ -38,15 +36,13 @@ class TestCrawler:
 
         crawler = StocksCrawler(ticker='BBDC4', source='https://br.investing.com/equities/bradesco-pn-n1-historical-data')
         result = crawler.get_last_daily_price()    
-        
-        assert len(result) == 1
-        
-        assert result[0].ticker == 'BBDC4'
-        assert result[0].open != 0.0
-        assert result[0].close != 0.0
-        assert result[0].high != 0.0
-        assert result[0].low != 0.0
-        assert result[0].low != 0 
+       
+        assert result.ticker == 'BBDC4'
+        assert result.open != 0.0
+        assert result.close != 0.0
+        assert result.high != 0.0
+        assert result.low != 0.0
+        assert result.low != 0 
         
 
     def test_get_history_brinvesting(self):
