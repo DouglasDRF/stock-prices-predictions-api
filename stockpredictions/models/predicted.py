be668101960626597399b1def0233cbfca004f6a
Douglas Ferreira <17039306+DouglasDRF@users.noreply.github.com>
2021-03-09 20:18:31 -0300

    Fix image entrypoint binding (#28)
    * Fix image entrypoint binding
    
    * Prediction endpoint fixes and operating on close
    
    * Replacing threading for high level asyncio

--- stockpredictions/models/predicted.py
+++ stockpredictions/models/predicted.py
@@ -1,8 +1,9 @@
 
 
 class Predicted:
-    def __init__(self, ticker, next_predicted_value, prediction_type, direction, date, real_value=None, real_direction=None):
+    def __init__(self, ticker, previous, next_predicted_value, prediction_type, direction, date, real_value=None, real_direction=None):
         self.ticker = ticker,
+        self.previous = previous,
         self.next_predicted_value = next_predicted_value,
         self.real_value = real_value,
         self.prediction_type = prediction_type,
@@ -11,6 +12,5 @@ class Predicted:
         self.date = date
 
     def to_dict(self):
-        result = {"ticker": self.ticker, "next_predicted_value": self.next_predicted_value, "real_value": self.real_value, "prediction_type":
-                  self.prediction_type, "direction": self.direction, "real_direction": self.real_direction, "date": self.date}
+        result = {"ticker": self.ticker, "previous": self.previous, "next_predicted_value": self.next_predicted_value, "real_value": self.real_value, "prediction_type": self.prediction_type, "direction": self.direction, "real_direction": self.real_direction, "date": self.date}
         return result
--- stockpredictions/services/coredata.py
+++ stockpredictions/services/coredata.py
@@ -1,6 +1,6 @@
 from stockpredictions.data import CoreDataRepository
 from stockpredictions.data import StocksCrawler
-import threading
+import asyncio
 
 class DataService:
 
@@ -11,30 +11,29 @@ class DataService:
         return  self.__repository.get_supported_stocks() 
     
     def update(self, ticker):
-        t = threading.Thread(target=self.__update, args=(ticker,), daemon=True)
-        t.start()
+        t = asyncio.create_task(self.__update(ticker))
         return { "status_message": "Update database task has been scheduled"}
 
     def fill_history(self, ticker):
-        t = threading.Thread(target=self.__fillTask, args=(ticker,))
-        t.start()
+        t = asyncio.create_task(self.__fillTask(ticker))
         return { "status_message": "Fill history task has been scheduled"}
 
-    def __update(self, ticker):
+    async def __update(self, ticker):
         try:
-            crawler = StocksCrawler(ticker, self.__repository.get_ticker_source(ticker))
-            last = crawler.get_last_daily_price()
-            self.__repository.save_last(last)
-            print("Scheduler update task ran sucessfully")
+            with StocksCrawler(ticker, self.__repository.get_ticker_source(ticker)) as crawler:
+                last = crawler.get_last_daily_price()
+                self.__repository.save_last(last)
+                print("Scheduler update task ran sucessfully")
         except Exception as e:
             print("Scheduled update task ran with error: \n" + str(e))
 
-    def __fillTask(self, ticker):
+    async def __fillTask(self, ticker):
         try:
-            crawler = StocksCrawler(ticker, self.__repository.get_ticker_source(ticker))
-            history = crawler.get_history()
-            self.__repository.save_to_history(history)
-            print("Scheduler fill history task ran sucessfully")
+            print('Initiating fiiling task')
+            with StocksCrawler(ticker, self.__repository.get_ticker_source(ticker)) as crawler:
+                history = crawler.get_history()
+                self.__repository.save_to_history(history)
+                print("Scheduler fill history task ran sucessfully")
         except Exception as e:
             print("Scheduled fill history task ran with error: \n" + str(e))
         
