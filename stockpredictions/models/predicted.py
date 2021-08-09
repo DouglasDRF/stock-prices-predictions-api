69ba562b2564edc529dbe83c68b9459e95e6a2e8
Douglas R. Ferreira <drfdouglas@outlook.com>
2021-08-09 20:33:36 -0300

    Migrating to DynamoDB

--- stockpredictions/models/predicted.py
+++ stockpredictions/models/predicted.py
@@ -1,10 +1,8 @@
-
-
 class Predicted:
-    def __init__(self, ticker, previous, next_predicted_value, prediction_type, direction, date, real_value=None, real_direction=None):
+    def __init__(self, ticker:str, previous, predicted_value, prediction_type:str, direction:str, date:str, real_value=None, real_direction=None):
         self.ticker = ticker,
         self.previous = previous,
-        self.next_predicted_value = next_predicted_value,
+        self.predicted_value = predicted_value,
         self.real_value = real_value,
         self.prediction_type = prediction_type,
         self.direction = direction,
@@ -12,5 +10,5 @@ class Predicted:
         self.date = date
 
     def to_dict(self):
-        result = {"ticker": self.ticker, "previous": self.previous, "next_predicted_value": self.next_predicted_value, "real_value": self.real_value, "prediction_type": self.prediction_type, "direction": self.direction, "real_direction": self.real_direction, "date": self.date}
+        result = {"ticker": self.ticker, "previous": self.previous, "next_predicted_value": self.predicted_value, "real_value": self.real_value, "prediction_type": self.prediction_type, "direction": self.direction, "real_direction": self.real_direction, "date": self.date}
         return result
--- stockpredictions/models/stock_price.py
+++ stockpredictions/models/stock_price.py
@@ -6,17 +6,16 @@ class StockPrice():
 
     def __init__(self, ticker='', timestamp=datetime.now(), opn=0, close=0, high=0, low=0, volume=''):
         self.ticker = ticker
-        self.timestamp = timestamp.isoformat() if type(timestamp) is datetime else timestamp
+        self.date = timestamp.strftime('%Y-%m-%d') if type(timestamp) is datetime else timestamp
         self.open = round(float(opn), 2)
         self.close = round(float(close), 2)
         self.high = round(float(high), 2)
         self.low = round(float(low), 2)
         self.volume = self.__parse_and_format_volume_value(volume)
-
         return
 
     def to_dict(self):
-        return {'ticker': self.ticker, 'timestamp': self.timestamp, 'open': self.open, 'close': self.close, 'high': self.high, 'low': self.close, 'volume': self.volume}
+        return {'ticker': self.ticker, 'timestamp': self.date, 'open': self.open, 'close': self.close, 'high': self.high, 'low': self.close, 'volume': self.volume}
 
     def __parse_and_format_volume_value(self, value):
         
