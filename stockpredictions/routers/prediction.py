15fb818453e0f358ce4266966c4780c433bb61d6
Douglas R. Ferreira <drfdouglas@outlook.com>
2022-01-31 19:03:24 -0300

    Fix save predict log

--- stockpredictions/routers/prediction.py
+++ stockpredictions/routers/prediction.py
@@ -5,9 +5,10 @@ from stockpredictions.services import PredictionService
 prediction_router = APIRouter()
 prediction_service = PredictionService()
 
+
 @prediction_router.get('/predict/nextday/{ticker}', tags=['Prediction'])
 async def predic_next_day(ticker: str, save_log: bool = False):
-    return { 'next_day_value': prediction_service.predict_next_day(ticker) }
+    return {'ticker': ticker, 'next_day_value': prediction_service.predict_next_day(ticker, save_log)}
 
 # @prediction_router.get('/predict/nextweek/{ticker}', tags=['Prediction'])
 # async def predic_next_week(ticker: str):
@@ -17,4 +18,4 @@ async def predic_next_day(ticker: str, save_log: bool = False):
 # @prediction_router.get('/predict/nextmonth/{ticker}', tags=['Prediction'])
 # async def predic_next_month(ticker: str):
 #     return { "Message" : "Not implemented"}
-#     # return prediction_service.predict_next_day(ticker) 
+#     # return prediction_service.predict_next_day(ticker)
